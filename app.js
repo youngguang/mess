f7762bd7e1fe31ddd8bbdf180dbb1d5483ac287e
刘阳光 <liuyangguang@ofcard.com>
2015-01-10 16:22:42 +0800

    提交

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var donate = require('./routes/donate');
+
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,10 +23,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/donate', donate);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
